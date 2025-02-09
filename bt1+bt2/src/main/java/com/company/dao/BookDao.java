c19198cbe778d60264088aef20a2c020f9aae809
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2025-02-09 11:28:04 +0700

    .

---
+++ bt1+bt2/src/main/java/com/company/dao/BookDao.java
@@ -0,0 +1,18 @@
+package com.company.dao;
+
+import java.util.List;
+import com.company.bean.Book;
+
+public interface BookDao {
+	void insertBook(Book book);
+
+	Book selectBook(int id);
+
+	List<Book> selectAllBooks();
+
+	boolean deleteBook(int id);
+
+	boolean updateBook(Book book);
+	
+	List<Book> searchBooksByTitle(String title);
+}
